2655951cc09b997311b3fa61912c4a4fe4d00c36
Mark Dinglasa <122752013+ryanmarkdinglasa@users.noreply.github.com>
2023-05-25 02:03:28 +0800

    Add files via upload

---
+++ student/print/fpdf/code/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Table with PHP and HTML</title>
+</head>
+<body>
+
+<form action="pdf.php" method="post">
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Age</th>
+      <th>City</th>
+    </tr>
+    <tr>
+      <td><input type="text" name="name1"></td>
+      <td><input type="text" name="age1"></td>
+      <td><input type="text" name="city1"></td>
+    </tr>
+    <tr>
+      <td><input type="text" name="name2"></td>
+      <td><input type="text" name="age2"></td>
+      <td><input type="text" name="city2"></td>
+    </tr>
+    <tr>
+      <td><input type="text" name="name3"></td>
+      <td><input type="text" name="age3"></td>
+      <td><input type="text" name="city3"></td>
+    </tr>
+  </table>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
